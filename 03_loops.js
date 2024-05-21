e440533de1ee3ef5d2598e9edabf0c9dd1467ea0
developerakashy <developerakashy@gmail.com>
2024-05-21 21:47:45 +0530

    LoopsInJs

---
+++ 03_loops.js
@@ -0,0 +1,23 @@
+for(let i = 0; i < 10; i++){
+    console.log("ForID" + i)
+}
+
+
+
+let start = 0
+let end = 10
+while (start < end){
+    start ++
+    console.log("WhileID"+start)
+}
+
+
+let init = 0
+let last = 10
+
+do{
+    init++
+    console.log("DoWhieID"+init)
+    
+}
+while(init < last)
